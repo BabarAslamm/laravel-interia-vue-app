aed59fe72f9c2fd1620d2f777c345cd6f16c260f
BabarAslamm <babaraslam825@gmail.com>
2024-08-24 13:31:36 +0500

    #11 Text Input Component

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, ref  } from "vue"
 import { useForm } from "@inertiajs/vue3";
+import  TextInput  from "../Components/TextInput.vue"
 
 
 const form = useForm({
@@ -32,27 +33,35 @@ const submit = () =>{
 
     <form @submit.prevent="submit">
         <div class="w-2/4 mx-auto">
-            <div class="mb-6">
-                <label for="">Name</label>
-                <input type="text" v-model="form.name" id="" autofocus>
-                <small v-if="form.errors.name" style="color: red;">{{ form.errors.name }}</small>
-            </div>
-            <div class="mb-6">
-                <label for="">Email</label>
-                <input type="email" v-model="form.email" id="" autocomplete="false">
-                <small v-if="form.errors.email" style="color: red;">{{ form.errors.email }}</small>
 
-            </div>
-            <div class="col-md-6">
-                <label for="">Password</label>
-                <input type="password" v-model="form.password" autocomplete="off">
-                <small v-if="form.errors.password" class="mb-1" style="color: red;">{{ form.errors.password }}</small>
+            <TextInput
+                name="name"
+                type="text"
+                v-model="form.name"
+                :message="form.errors.name"
+            />
 
-            </div>
-            <div class="mb-6">
-                <label for="">Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation">
-            </div>
+            <TextInput
+                name="Email"
+                type="email"
+                v-model="form.email"
+                :message="form.errors.email"
+            />
+
+            <TextInput
+                name="Password"
+                type="password"
+                v-model="form.password"
+                :message="form.errors.password"
+            />
+
+            <TextInput
+                name="Confirm Password"
+                type="password"
+                v-model="form.password_confirmation"
+            />
+
+
             <div class="mb-6">
                 <p class="text-slate-600 mb-2">Already have a account? <a href="#" class="text-link"> Login</a></p>
 
---
+++ resources/js/Pages/Components/TextInput.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, defineProps } from 'vue';
+const model = defineModel({
+    type: null,
+    required: true,
+
+});
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: "text",
+    },
+    message: String,
+
+})
+</script>
+
+<template>
+<div class="mb-6">
+    <label for="">{{ name}}</label>
+    <input :type="type" v-model="model" id="" :class="{ '!ring-red-500' : message}" autofocus>
+    <small v-if="message" class="error">{{ message }}</small>
+</div>
+</template>
